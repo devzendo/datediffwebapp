44ec920a5e73282e4056253744466d58ff5a4aaa
matt.gumbley <unknown>
2012-10-05 08:08:54 +0100

    Checkpoint of progress/experiments so far; hard-coded sample data, list of groups and dates within each group are populated. Date edit dialog started.

---
+++ datediff.js
@@ -0,0 +1,59 @@
+
+var groups = [];
+
+function startup()
+{
+	loadData();
+	showGroups();
+}
+
+function loadData()
+{
+    groups = [ 
+	   "Birthdays", "CV Durations", "Accommodation", "Business",
+        "Alive since", "Another", "Yet another", "More", "Endless", 
+	   "Dates I forget","Embarrassing", "Isn't it?"
+    ];
+	groupedDates = [
+	   /* Birthdays */ [ "Matt", "Angela", "Natasha", "Hannah", "Thomas"],
+	   /* CV Durations */ [ "Known C", "Known C++", "Known Perl", "Known UNIX" ],
+	   /* Accommodation */ [ "Porthill", "Alsagers Bank", "Wolstanton", "Keele", "Westlands"],
+	   /* Business */ [ "Synchro", "Keele", "Enigma", "Inspired", "Resilient"],
+	   // etc, etc
+	];	
+}
+
+function showGroups()
+{
+   	var newGrHtml = '<li class="arrow">' + 
+	   groups.map(function(item, indx){
+           return '<a href="#dates" onClick="showDates(' + indx + ')" >' + item + '</a>';
+       }).join("") +
+	   '</li>'; 
+	$("ul#groups").find("li.arrow").replaceWith(newGrHtml);
+}
+
+function showDates(groupIndex)
+{
+    var newHtml = '<li class="arrow">' + 
+       groupedDates[groupIndex].map(function(item, indx){
+           return '<a href="#datePanel" onClick="showDate(' + groupIndex + ',' + indx + ')" >' + item + '</a>';
+       }).join("") +
+       '</li>'; 
+    $("ul#dates").find("li.arrow").replaceWith(newHtml);
+    $("ul#dates").attr("title", groups[groupIndex]);
+
+}
+
+function showDate(groupIndex, dateIndex)
+{
+	var title = groupedDates[groupIndex][dateIndex]
+    $("div#datePanel").attr("title", title);
+	$("div#datePanel fieldset div input#dateName").attr("value", title);
+}
+
+function hidePrefsButton()
+{
+	alert("hi");
+	$("div.toolbar").find("a#prefsButton").style.display="none";
+}
